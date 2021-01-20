855c124280c16472de188ae7578c578eefcd68fd
bagusshndr <bagussuhendri19@gmail.com>
2021-01-20 22:12:19 +0700

    PUSH

--- application/views/VAbout.php
+++ application/views/VAbout.php
@@ -6,29 +6,47 @@
              <h4 class="my-3" style="float: left;">Sejarah Kami</h4>
              <br>
              <br>
-             <p style="color: black; text-align: justify;">WberPack berdiri pada Tahun 2017. WberPack sendiri didirikan oleh 3 orang profesional yang memiliki pengalaman dibidang paper packaging dan bidang finance selama 16 tahun.
-                 Bermula ketika pendiri perusahaan pernah melakukan invest pasif pada bidang karton box namun menemui kegagalan. Setelah mengalami kegagalan, pendiri perusahaan mencoba memanfaatkan
-                 yang ada dengan keahlian pada bidang packaging dan paper core. Para pendiri perusahaan memulai bisnis ini dari awal dan memutuskan mendirikan sebuah perusahaan.
+             <p style="color: black; text-align: justify;">WberPack berdiri pada Tahun 2017. WberPack sendiri didirikan
+                 oleh 3 orang profesional yang memiliki pengalaman dibidang paper packaging dan bidang finance selama 16
+                 tahun.
+                 Bermula ketika pendiri perusahaan pernah melakukan invest pasif pada bidang karton box namun menemui
+                 kegagalan. Setelah mengalami kegagalan, pendiri perusahaan mencoba memanfaatkan
+                 yang ada dengan keahlian pada bidang packaging dan paper core. Para pendiri perusahaan memulai bisnis
+                 ini dari awal dan memutuskan mendirikan sebuah perusahaan.
                  Terbentuk PT. Wahana Bermuda Nusantara dan dikenal sebagai WBN yang sekarang menjadi WberPack.
              </p>
              <br>
-             <p style="color: black; text-align: justify;">Pada awal berdiri PT. Wahana Bermuda Nusantara beroperasi gudang kecil di daerah Kerangan dengan
-                 menggunakan 1 mesin dan hanya terdapat 4 orang karyawan, karena keterbatasan tenaga kerja pendiri perusahaan juga turut ikut
-                 serta dalam memproduksi pesanan pelanggan. Pada awal berdiri WberPack hanya mendapatkan penghasilan 6 juta rupiah perbulan.
-                 Di Tahun kedua WberPack sudah memiliki 45 karyawan dan mampu menorehkan penjualan hingga 825 juta rupiah perbulan dimana
-                 sudah ada 21 pelanggan tetap yang mempercayai WberPack sebagai pemasok sebagai produk pendukung untuk perusahaan mereka.
-                 Seiring dengan berjalannya waktu, saat ini WberPack juga sudah memiliki 80 karyawan dengan waktu produksi mencapai 24 jam dan
-                 mampu menghasilkan 3 Ton produksi per hari atau sekitar 21.000 produksi dalam sehari.produk dalam sehari. Saat ini WberPack juga terus berkembang dengan peningkatan penjualan yang terjadi secara
-                 konsisten setiap tahun dan telah menghasilkan penjualan hingga mencapai angka 1.7 Milyar Rupiah perbulan. Bahkan mampu bertahan pada saat terjadi pandemi Covid 19.
+             <p style="color: black; text-align: justify;">Pada awal berdiri PT. Wahana Bermuda Nusantara beroperasi
+                 gudang kecil di daerah Kerangan dengan
+                 menggunakan 1 mesin dan hanya terdapat 4 orang karyawan, karena keterbatasan tenaga kerja pendiri
+                 perusahaan juga turut ikut
+                 serta dalam memproduksi pesanan pelanggan. Pada awal berdiri WberPack hanya mendapatkan penghasilan 6
+                 juta rupiah perbulan.
+                 Di Tahun kedua WberPack sudah memiliki 45 karyawan dan mampu menorehkan penjualan hingga 825 juta
+                 rupiah perbulan dimana
+                 sudah ada 21 pelanggan tetap yang mempercayai WberPack sebagai pemasok sebagai produk pendukung untuk
+                 perusahaan mereka.
+                 Seiring dengan berjalannya waktu, saat ini WberPack juga sudah memiliki 80 karyawan dengan waktu
+                 produksi mencapai 24 jam dan
+                 mampu menghasilkan 3 Ton produksi per hari atau sekitar 21.000 produksi dalam sehari.produk dalam
+                 sehari. Saat ini WberPack juga terus berkembang dengan peningkatan penjualan yang terjadi secara
+                 konsisten setiap tahun dan telah menghasilkan penjualan hingga mencapai angka 1.7 Milyar Rupiah
+                 perbulan. Bahkan mampu bertahan pada saat terjadi pandemi Covid 19.
              </p>
              <br>
-             <p style="color: black; text-align: justify;">Setelah berjalan dari tahun 2017 PT. Wahana Bermuda Nusantara yang sebelum nya dikenal dengan WBN pada tahun 2020, memutuskan untuk melakukan
-                 melakukan rebranding menjadi WberPack termasuk logo baru yang lebih memiliki karakter dan mudah dikenal. Dimana kami ingin menguatkan brand image kami dan membuktikan
+             <p style="color: black; text-align: justify;">Setelah berjalan dari tahun 2017 PT. Wahana Bermuda Nusantara
+                 yang sebelum nya dikenal dengan WBN pada tahun 2020, memutuskan untuk melakukan
+                 melakukan rebranding menjadi WberPack termasuk logo baru yang lebih memiliki karakter dan mudah
+                 dikenal. Dimana kami ingin menguatkan brand image kami dan membuktikan
                  bahwa perusahaan kami serius bergerak dalam industri paper core.
-                 WberPack (PT. Wahana Bermuda Nusantara) memiliki produk utama yaitu Paper Core. Paper Core menjadi produk yang bernilai tinggi dan paling banyak diproduksi saat ini.
-                 Dimana produk paper core dikelompokkan dalam beberapa kategori antara lain Paper Core dengan jenis Brown Kraft, White Kraft, dan Non-Craft. Produk paper core dapat disesuaikan
-                 mulai dari ukuran diameter luar dan dalam, ketebalan, panjang, pencetakan logo pada bagian luar maupun dalam paper core dengan sablon, dan menentukan bahan yang akan digunakan sesuai dengan kebutuhan.
-                 WberPack juga memproduksi 6 jenis produk paper seperti Honeycomb, Karton Box, Layer Bundar, Paper Core Celengan, Paper Pallet, dan Edge Protector.
+                 WberPack (PT. Wahana Bermuda Nusantara) memiliki produk utama yaitu Paper Core. Paper Core menjadi
+                 produk yang bernilai tinggi dan paling banyak diproduksi saat ini.
+                 Dimana produk paper core dikelompokkan dalam beberapa kategori antara lain Paper Core dengan jenis
+                 Brown Kraft, White Kraft, dan Non-Craft. Produk paper core dapat disesuaikan
+                 mulai dari ukuran diameter luar dan dalam, ketebalan, panjang, pencetakan logo pada bagian luar maupun
+                 dalam paper core dengan sablon, dan menentukan bahan yang akan digunakan sesuai dengan kebutuhan.
+                 WberPack juga memproduksi 6 jenis produk paper seperti Honeycomb, Karton Box, Layer Bundar, Paper Core
+                 Celengan, Paper Pallet, dan Edge Protector.
              </p>
          </div>
 
@@ -37,8 +55,11 @@
                  <div class="skills-content">
 
                      <h4 class="my-3">Visi</h4>
-                     <p style="color: black; text-align: justify;">Menjadi perusahaan packaging yang berkembang, dipercaya dan mampu menciptakan nilai tambah bagi pelanggan, serta mampu meningkatkan sumber daya Perusahaan. Kedepannya kami akan terus melakukan inovasi
-                         untuk memproduksi berbagai macam jenis packaging yang ada dipasaran dimana memegang peranan menjadi market leader dengan kualitas berstandar Internasional.
+                     <p style="color: black; text-align: justify;">Menjadi perusahaan packaging yang berkembang,
+                         dipercaya dan mampu menciptakan nilai tambah bagi pelanggan, serta mampu meningkatkan sumber
+                         daya Perusahaan. Kedepannya kami akan terus melakukan inovasi
+                         untuk memproduksi berbagai macam jenis packaging yang ada dipasaran dimana memegang peranan
+                         menjadi market leader dengan kualitas berstandar Internasional.
                      </p>
                  </div>
              </div>
@@ -57,8 +78,16 @@
              <div class="col-lg-6 pt-4 pt-lg-0 content">
                  <div class="skills-content">
                      <h4 class="my-3">Misi</h4>
-                     <p style="color: black; text-align: justify;">WberPack senantiasa mengedepankan jaminan kualitas dari setiap produk kepada pelanggan. Membangun kepercayaan bisnis melalui kerjasama dengan perusahaan-perusahaan yang terpercaya dan berdedikasi tinggi dalam menciptakan produk. Kami terus meningkatkan kualitas sumber daya manusia dan mengutamakan kesejahteraan tenaga kerja.
-                         WberPack semakin memperkuat citra merek dengan menyiapkan strategi dan ide-ide yang inovatif sesuai dengan kebutuhan dan selera pelanggan, serta konsisten dengan mempersembahkan layanan yang berkualitas. Kami juga memastikan keuntungan kepada pemilik perusahaan dan tetap membantu usaha lain untuk terus tetap berkembang melalui kerjasama yang dijalin. Kami juga berkomitmen untuk menjaga lingkungan sekitar dengan bahan produk yang tidak merusak lingkungan dan pengolahan limbah melalui proses daur ulang yang ketat dan ramah lingkungan.
+                     <p style="color: black; text-align: justify;">WberPack senantiasa mengedepankan jaminan kualitas
+                         dari setiap produk kepada pelanggan. Membangun kepercayaan bisnis melalui kerjasama dengan
+                         perusahaan-perusahaan yang terpercaya dan berdedikasi tinggi dalam menciptakan produk. Kami
+                         terus meningkatkan kualitas sumber daya manusia dan mengutamakan kesejahteraan tenaga kerja.
+                         WberPack semakin memperkuat citra merek dengan menyiapkan strategi dan ide-ide yang inovatif
+                         sesuai dengan kebutuhan dan selera pelanggan, serta konsisten dengan mempersembahkan layanan
+                         yang berkualitas. Kami juga memastikan keuntungan kepada pemilik perusahaan dan tetap membantu
+                         usaha lain untuk terus tetap berkembang melalui kerjasama yang dijalin. Kami juga berkomitmen
+                         untuk menjaga lingkungan sekitar dengan bahan produk yang tidak merusak lingkungan dan
+                         pengolahan limbah melalui proses daur ulang yang ketat dan ramah lingkungan.
                      </p>
                  </div>
              </div>
@@ -71,7 +100,13 @@
              <div class="col-lg-6 pt-4 pt-lg-0 content">
                  <div class="skills-content">
                      <h4 class="my-3">Komitmen</h4>
-                     <p style="color: black; text-align: justify;">Komitmen WberPack adalah mempertahankan visi kedepannya sebagai pemimpin industri packaging dengan terus memberikan yang terbaik kepada pelanggan. Dukungan pemilik perusahaan, keahlian manajemen yang dikelola dengan baik, dedikasi, integritas serta loyalitas tenaga kerja. Dan yang terpenting adalah, kesinambungan kerjasama antara pelanggan, pasti berhasil membawa WberPack meraih visinya. Dengan visi dan misi yang kuat dan nilai-nilai perusahaan yang jelas, tentu akan terus menjadikan perusahaan WberPack yang terfavorit di seluruh Indonesia.
+                     <p style="color: black; text-align: justify;">Komitmen WberPack adalah mempertahankan visi
+                         kedepannya sebagai pemimpin industri packaging dengan terus memberikan yang terbaik kepada
+                         pelanggan. Dukungan pemilik perusahaan, keahlian manajemen yang dikelola dengan baik, dedikasi,
+                         integritas serta loyalitas tenaga kerja. Dan yang terpenting adalah, kesinambungan kerjasama
+                         antara pelanggan, pasti berhasil membawa WberPack meraih visinya. Dengan visi dan misi yang
+                         kuat dan nilai-nilai perusahaan yang jelas, tentu akan terus menjadikan perusahaan WberPack
+                         yang terfavorit di seluruh Indonesia.
                      </p>
                  </div>
              </div>
@@ -106,14 +141,42 @@
                      <img src="<?php echo base_url('assets/img/team/team-1.jpg'); ?>" class="img-fluid" alt="">
                      <div class="member-info">
                          <div class="member-info-content">
-                             <h4>Sarah Jhonson</h4>
-                             <span>Product Manager</span>
+                             <div class="row">
+                                 <div class="col-lg-4">
+                                     <p>
+                                         p
+                                     </p>
+                                 </div>
+                                 <div class="col-lg-4">
+                                     <p>
+                                         p
+                                     </p>
+                                 </div>
+                                 <div class="col-lg-4">
+                                     <p>
+                                         p
+                                     </p>
+                                 </div>
+                             </div>
                          </div>
                          <div class="social">
-                             <a href=""><i class="icofont-twitter"></i></a>
-                             <a href=""><i class="icofont-facebook"></i></a>
-                             <a href=""><i class="icofont-instagram"></i></a>
-                             <a href=""><i class="icofont-linkedin"></i></a>
+                             <div class="row">
+                                 <div class="col-lg-4">
+                                     <p>
+                                         p
+                                     </p>
+                                 </div>
+                                 <div class="col-lg-4">
+                                     <p>
+                                         p
+                                     </p>
+                                 </div>
+                                 <div class="col-lg-4">
+                                     <p>
+                                         p
+                                     </p>
+                                 </div>
+                             </div>
                          </div>
                      </div>
                  </div>
